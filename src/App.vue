1e74ac9964f535a9c2af499d557128d43cb2116c
Krystian Nowakowski <krysti.8642@gmail.com>
2022-06-17 16:54:26 +0200

    login component restructure

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-import NavBar from "./components/NavBar.vue";
 import { user } from "./setup";
-import RegistrationForm from "./components/RegistrationForm.vue";
-import LoginForm from "./components/LoginForm.vue";
+import NavBar from "./components/NavBar.vue";
+import NoLogin from "./components/NoLogin.vue";
 import TaskList from "./components/TaskList.vue";
 </script>
 
 <template>
     <NavBar />
-    <div v-if="user.name == ''">
-        <LoginForm />
-        <RegistrationForm />
-    </div>
-    <div v-if="user.name != ''">
-        <TaskList />
-    </div>
+    <NoLogin v-if="user.name == ''" />
+    <TaskList v-if="user.name != ''" />
 </template>
 
 <style>
---
+++ src/components/NoLogin.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import RegistrationForm from "./RegistrationForm.vue";
+import LoginForm from "./LoginForm.vue";
+</script>
+
+<template>
+    <LoginForm />
+    <RegistrationForm />
+</template>

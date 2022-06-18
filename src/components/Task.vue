2e00ba9c9b779d2b1675c298c2157b0e92cf6646
Krystian Nowakowski <krysti.8642@gmail.com>
2022-06-18 14:55:50 +0200

    task style

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
-import { ITask, blankTask } from "../interfaces/Task";
+import { computed } from "@vue/reactivity";
+import { ITask } from "../interfaces/Task";
 const props = defineProps<{
     task: ITask;
 }>();
+const time_string = (timestamp: number) => {
+    const date = new Date(timestamp);
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+};
 </script>
 
 <template>
     <div class="bg-indigo-100 m-4 p-4 rounded">
-        <p class="text-2xl">{{ task.name }}</p>
+        <p class="text-xs font-extralight text-slate-500">
+            {{ time_string(task.created_at) }}
+        </p>
+        <p class="text-3xl">{{ task.name }}</p>
         <p>{{ task.desc }}</p>
-        <p>{{ task.done }}</p>
-        <p>{{ task.created_at }}</p>
+        <button>{{ task.done ? "Done" : "Not Done" }}</button>
     </div>
 </template>
